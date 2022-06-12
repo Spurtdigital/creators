1fae09fc709dc81a9872c8d445d5e92f7a22fac5
Spurtdigital <info@spurt.digital>
2022-06-12 13:54:09 +0200

    🐛 update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-~Current Version:0.3.0~
+~Current Version:0.4.0~
 
 # Creators
 Base setup for creator theme's
--- creators.php
+++ creators.php
@@ -2,8 +2,8 @@
 /**
  * Plugin Name: Creators
  * Plugin URI: https://github.com/Spurtdigital/creators
- * Description: Release 0.3.0
- * Version: 0.3.0
+ * Description: Release 0.4.0
+ * Version: 0.4.0
  * Author: Kevin Brinkman
  * License: GNU General Public License v2
  * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
